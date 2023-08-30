3622cb0a530ff7531b2dc6e68d11bcd9aec01234
Tez <tlatocateculti@gmail.com>
2023-08-30 20:28:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def zamien(*x, szukane, zamiana, ilosc = -1, start=-1):
+    ret = ""
+    for ciag in x:
+        strTmp = str(ciag)
+        for slowo in strTmp.split(' '):
+            indexZnajdz =-1
+            while True:
+                indexZnajdz = slowo.find(szukane, indexZnajdz+1) if indexZnajdz != -1 else -1
+                if indexZnajdz != -1 and (ilosc == -1 or ilosc > 0) and start==-1:
+                    slowo = slowo[:indexZnajdz] + zamiana + slowo[indexZnajdz+len(szukane):]
+                    ilosc -= 1 if ilosc > 0 else 0
+
+                elif start> -1:
+                    start-=1
+                    continue
+                else: break
+            ret += slowo + ' '
+    return ret
+
+
+ciag = 45665758833
+mStr = zamien(ciag, " To kolejny przypadek", 5672213, "Dalsze 66566 wiadomości", szukane='6', zamiana='1', ilosc = 4, start=2)
+print(mStr)
